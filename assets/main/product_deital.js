a5fda6d7986793228749d2ffcc3d827e8f471e05
buianhtuan2207 <22130309@st.hcmuaf.edu.vn>
2024-11-02 01:08:41 +0700

    thêm btn_color_img_product vào ctsp

--- assets/main/product_deital.js
+++ assets/main/product_deital.js
@@ -11,3 +11,22 @@ btnLeft.addEventListener('click', function(){
     let imgs = document.querySelectorAll('.img_product') 
     document.querySelector('.img_slide').prepend(imgs[imgs.length - 1])
 });
+
+const buttons = {
+    btnPurple: document.querySelector('.btn_purple'),
+    btnPink: document.querySelector('.btn_pink'),
+    btnCream: document.querySelector('.btn_cream'),
+    btnGreen: document.querySelector('.btn_green')
+};
+
+const imgSlide = document.querySelector('.img_slide');
+let imgs = document.querySelectorAll('.img_product');
+
+Object.entries(buttons).forEach(([key, button], index) => {
+    button.addEventListener('click', function() {
+        imgSlide.innerHTML = ''; // xóa ảnh hiện tại
+        imgSlide.appendChild(imgs[index]); // thêm ảnh
+    });
+});
+
+
