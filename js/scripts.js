a5a9d8722adf120fa038faf334276a0eb0601d52
nguyenvanan040804 <22130003@st.hcmuaf.edu.vn>
2024-11-13 21:22:03 +0700

    demo trang admin

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+// Thêm hành vi JavaScript cho các tính năng bổ sung nếu cần
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Admin dashboard loaded");
+});
