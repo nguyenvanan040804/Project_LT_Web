6f7697fa3f96b3e6b9b05412888601526067b147
haodo04 <22130077@st.hcmuaf.edu.vn>
2024-11-13 17:30:52 +0700

    them thong bao uu dai, giam gia qua email va button dau trang

--- assets/main/script.js
+++ assets/main/script.js
@@ -123,3 +123,12 @@ document.addEventListener('click', (e) => {
         divPopup.classList.remove('show')
     }
 })
+
+
+// show modal on load 
+window.onload = function() {
+    document.querySelector('.site').classList.toggle('showmodal')
+}
+document.querySelector('.modalclose').addEventListener('click', function() {
+    document.querySelector('.site').classList.remove('showmodal')
+})
