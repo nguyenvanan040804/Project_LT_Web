6dbbef980f0181f707cb6ffd9d1fba2b8a1de03f
nguyenvanan040804 <22130003@st.hcmuaf.edu.vn>
2024-11-16 23:29:38 +0700

    xong trang san pham balo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Admin dashboard loaded");
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("toggle-btn");
+    const extraText = document.querySelector(".extra-text");
+  
+    toggleBtn.addEventListener("click", function () {
+        const isHidden = extraText.style.display === "none" || extraText.style.display === "";
+  
+        if (isHidden) {
+            extraText.style.display = "inline";
+            toggleBtn.textContent = "Thu gọn";
+        } else {
+            extraText.style.display = "none";
+            toggleBtn.textContent = "Xem thêm";
+        }
+    });
+  });
+  
