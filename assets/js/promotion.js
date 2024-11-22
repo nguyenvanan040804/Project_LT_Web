e13c389c98f5f8179925a391bc558f8c52fef33a
unknown <22130003@st.hcmuaf.edu.vn>
2024-11-22 03:23:25 -0500

    xong khung mau cho phan khuyen mai xe dap dia hinh

--- assets/js/promotion.js
+++ assets/js/promotion.js
@@ -1,6 +1,8 @@
 // Select dots and slider wrapper
 const dots = document.querySelectorAll('.dot');
+const dots1 = document.querySelectorAll('.dot1');
 const sliderWrapper = document.querySelector('.slider-wrapper');
+const sliderWrapper1 = document.querySelector('.slider-wrapper1');
 
 // Add click event to each dot
 dots.forEach((dot, index) => {
@@ -13,5 +15,20 @@ dots.forEach((dot, index) => {
 
     // Update slider position
     sliderWrapper.style.transform = `translateX(-${index * 1205}px)`;
+  });
+});
+
+
+
+dots1.forEach((dot1, index) => {
+  dot1.addEventListener('click', () => {
+    // Remove active class from all dots
+    dots1.forEach(d => d.classList.remove('active'));
+
+    // Add active class to the clicked dot
+    dot1.classList.add('active');
+
+    // Update slider position
+    sliderWrapper1.style.transform = `translateX(-${index * 1205}px)`;
   });
 });
