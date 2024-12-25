346c01b8bfc7090484697a723b0c8921a52775a9
haodo04 <22130077@st.hcmuaf.edu.vn>
2024-12-25 19:23:07 +0700

    xong ham xoa sua lay theo id cua cate

--- src/main/java/vn/edu/hcmuaf/fit/demo/dao/impl/CategoryDaoImpl.java
+++ src/main/java/vn/edu/hcmuaf/fit/demo/dao/impl/CategoryDaoImpl.java
@@ -47,5 +47,70 @@ public class CategoryDaoImpl implements ICategoryDao {
         return categories;
     }
 
+    @Override
+    public Category getCateById(int id) {
+        String sql = "select * from categories where id = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                return new Category(rs.getInt("id"), rs.getString("cateName"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    @Override
+    public boolean deleteCateById(int id) {
+        String sql = "delete from categories where id = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateCate(Category cate) {
+        String sql = "update categories set cateName = ? where id = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, cate.getId());
+            ps.setString(2, cate.getCateName());
+            return ps.executeUpdate() > 0;
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        CategoryDaoImpl categoryDao = new CategoryDaoImpl(DBConnect.getConnect());
+//        Category cate = new Category(2, "Xe Đạp Thể Thao");
+//        Category cate1 = new Category(3, "Xe Đạp Địa Hình");
+//        Category cate2 = new Category(4, "Xe Đạp Touring");
+//        Category cate3 = new Category(5, "Xe Đạp Đua");
+//        Category cate4 = new Category(6, "Xe Đạp Điện");
+//        Category cate5 = new Category(7, "Giảm Giá Ưu Đãi");
+//        Category cate6 = new Category(8, "Xe Đạp Phổ Thông");
+//        Category cate7 = new Category(9, "Phụ Kiện Xe Đạp");
+//        Category cate8 = new Category(10, "Thương Hiệu Xe Đạp");
+//        Category cate9 = new Category(11, "Bán Chạy Nhất");
+//        categoryDao.addCategory(cate);
+//        categoryDao.addCategory(cate1);
+//        categoryDao.addCategory(cate2);
+//        categoryDao.addCategory(cate3);
+//        categoryDao.addCategory(cate4);
+//        categoryDao.addCategory(cate5);
+//        categoryDao.addCategory(cate6);
+//        categoryDao.addCategory(cate7);
+//        categoryDao.addCategory(cate8);
+//        categoryDao.addCategory(cate9);
+    }
 }
