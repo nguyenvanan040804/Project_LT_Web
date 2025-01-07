e61807545eadc0c0ab105f4b5774c57050f752f2
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-01-07 23:30:48 +0700

    xong ham email lai mat khau

--- src/main/java/vn/edu/hcmuaf/fit/demo/utils/Email.java
+++ src/main/java/vn/edu/hcmuaf/fit/demo/utils/Email.java
@@ -57,7 +57,55 @@ public class Email {
         }
         return test;
     }
+    public boolean emailSend(User user) {
+        boolean test = false;
+        String toEmail = user.getEmail();
+        String fromEmail = "clanbatluc22@gmail.com";
+        String password = "fdpm awfa djmt tqyb";
 
+        try {
+            // your host email smtp server details
+            Properties pr = configEmail(new Properties());
 
+            // get session to authenticate the host email address and password
+            Session session = Session.getInstance(pr, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(fromEmail, password);
+                }
+            });
+            Message mess = new MimeMessage(session);
+            mess.setHeader("Content-Type", "text/plain; charset=UTF-8");
+            // set from email address
+            mess.setFrom(new InternetAddress(fromEmail, "BikeStore Cửa Hàng Xe Đạp"));
+
+            // thiết lập email người nhận
+            mess.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+
+            // set email subject
+            mess.setSubject("Your Password");
+
+            // set message text
+            mess.setText("Your password is: " + user.getPassWord());
+            // send the message
+            Transport.send(mess);
+            test = true;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        return test;
+    }
+
+    public Properties configEmail(Properties pr) {
+        pr.setProperty("mail.smtp.host", "smtp.gmail.com");
+        pr.setProperty("mail.smtp.port", "587");
+        pr.setProperty("mail.smtp.auth", "true");
+        pr.setProperty("mail.smtp.starttls.enable", "true");
+        pr.put("mail.smtp.socketFactory.port", "587");
+        pr.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        return pr;
+    }
 
 }
