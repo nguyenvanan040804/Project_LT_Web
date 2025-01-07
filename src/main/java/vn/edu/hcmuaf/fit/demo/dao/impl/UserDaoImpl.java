a4f3d871186816f55189412aa1019114a7bc2346
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-01-07 23:37:35 +0700

    cac ham insert va check du lieu cua user dao

--- src/main/java/vn/edu/hcmuaf/fit/demo/dao/impl/UserDaoImpl.java
+++ src/main/java/vn/edu/hcmuaf/fit/demo/dao/impl/UserDaoImpl.java
@@ -23,7 +23,82 @@ public class UserDaoImpl implements IUserDao {
         this.conn = conn;
     }
 
+    @Override
+    public void insertRegister(User user) {
+        String sql = "insert into users (roleId, userName, fullName, passWord, email, phone, address, status, code) values (?, ?, " +
+                "?, ?, ?, ?, ?, ?, ?)";
+        try {
+            conn = DBConnect.getConnect();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, user.getRoleId());
+            ps.setString(2, user.getUserName());
+            ps.setString(3, user.getFullName());
+            ps.setString(4, user.getPassWord());
+            ps.setString(5, user.getEmail());
+            ps.setString(6, user.getPhone());
+            ps.setString(7, user.getAddress());
+            ps.setInt(8, user.getStatus());
+            ps.setString(9, user.getCode());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    @Override
+    public void updateStatus(User user) {
+        String sql = "update users set status = ?, code = ? where email  = ?";
+        try {
+            conn = DBConnect.getConnect();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, user.getStatus());
+            ps.setString(2, user.getCode());
+            ps.setString(3, user.getEmail());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean checkExistEmail(String email) {
+        boolean duplicate = false;
+        String sql = "select * from users where email = ?";
+        try {
+            conn = DBConnect.getConnect();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                duplicate = true;
+            }
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return duplicate;
+    }
+
+    @Override
+    public boolean checkExistUsername(String username) {
+        boolean duplicate = false;
+        String sql = "select * from users where userName = ?";
+        try {
+            conn = DBConnect.getConnect();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                duplicate = true;
+            }
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return duplicate;
+    }
 
     @Override
     public List<User> findAll() {
