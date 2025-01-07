fe615c26b596b41485353e1fc77d307e2a8a4d0a
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-01-07 23:35:50 +0700

    xong ham sua xoa trong user dao

--- src/main/java/vn/edu/hcmuaf/fit/demo/dao/impl/UserDaoImpl.java
+++ src/main/java/vn/edu/hcmuaf/fit/demo/dao/impl/UserDaoImpl.java
@@ -23,80 +23,58 @@ public class UserDaoImpl implements IUserDao {
         this.conn = conn;
     }
 
+
+
     @Override
-    public void insertRegister(User user) {
-        String sql = "insert into users (roleId, userName, fullName, passWord, email, phone, address, status, code) values (?, ?, " +
-                "?, ?, ?, ?, ?, ?, ?)";
+    public void update(User user) {
+        String sql = "update users set roleId = ?, userName = ?, fullName = ?, email = ?," +
+                " passWord = ?, phone = ?, address = ?, status = ?, code = ? where id = ?";
         try {
-            conn = DBConnect.getConnect();
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setInt(1, user.getRoleId());
             ps.setString(2, user.getUserName());
             ps.setString(3, user.getFullName());
-            ps.setString(4, user.getPassWord());
-            ps.setString(5, user.getEmail());
+            ps.setString(4, user.getEmail());
+            ps.setString(5, user.getPassWord());
             ps.setString(6, user.getPhone());
             ps.setString(7, user.getAddress());
             ps.setInt(8, user.getStatus());
             ps.setString(9, user.getCode());
             ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        }catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
     @Override
-    public void updateStatus(User user) {
-        String sql = "update users set status = ?, code = ? where email  = ?";
+    public void delete(int id) {
+        String sql = "delete from users where id = ?";
         try {
-            conn = DBConnect.getConnect();
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setInt(1, user.getStatus());
-            ps.setString(2, user.getCode());
-            ps.setString(3, user.getEmail());
+            ps.setInt(1, id);
             ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        }catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    @Override
-    public boolean checkExistEmail(String email) {
-        boolean duplicate = false;
-        String sql = "select * from users where email = ?";
+    public User findByUserNameAndEmail(String username, String email) {
+        User user = null;
+        String sql = "select * from users where username = ? and email = ?";
         try {
-            conn = DBConnect.getConnect();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, email);
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()) {
-                duplicate = true;
-            }
-            ps.close();
-            conn.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return duplicate;
-    }
-
-    @Override
-    public boolean checkExistUsername(String username) {
-        boolean duplicate = false;
-        String sql = "select * from users where userName = ?";
-        try {
-            conn = DBConnect.getConnect();
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, username);
+            ps.setString(2, email);
             ResultSet rs = ps.executeQuery();
             if(rs.next()) {
-                duplicate = true;
+                user = new User();
+                user.setUserName(rs.getString("username"));
+                user.setEmail(rs.getString("email"));
+                user.setPassWord(rs.getString("password"));
             }
-            ps.close();
-            conn.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        }catch (SQLException e) {
+            e.printStackTrace();
         }
-        return duplicate;
+        return user;
     }
-
+}
